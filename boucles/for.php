4b80083b41d539bfb563d4781a6997c241a69d31
Carole <carole.velarde@orange.fr>
2017-03-28 12:15:35 +0200

    Exercices terminés

--- boucles/for.php
+++ boucles/for.php
@@ -111,17 +111,20 @@ echo '<br><br>';
 //Créer un tableau pour les mois de l'année et affiché tous les mois de Janvier à Décembre
 //modifier et/ou remplacer les éléments ci-dessous
 echo "8.Les mois depuis le debut de l'annee : <br>";
-$mois = [];
-for ($i = 0; $i < 0; $i) {
-    echo '';
+$mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+foreach ($mois as $month) {
+    echo $month."\n";
 }
 echo '<br><br>';
 
 //Afficher les mois de la fin de l'année jusqu'au début de l'année
 //modifier et/ou remplacer les éléments ci-dessous
 echo "9.Les mois depuis la fin de l'annee : <br>";
-for ($i = 0; $i < 0; $i) {
-    echo '';
+for ($i = 12; $i >= 0; $i--) {
+    echo $mois[$i]."\n";
+}
+foreach(array_reverse($mois) as $month){
+  echo $month."\n";
 }
 echo '<br><br>';
 //----------------------------------------
@@ -145,10 +148,25 @@ $college = array(
     array('Nom' => 'Johannson', 'Prenom' => 'Scarlett'),
     array('Nom' => 'Jackson', 'Prenom' => 'Samuel'),
   ),
-);
+  "Troisième" => array(
+    array("Nom" => "Neimare", "Prenom" => "Jean"),
+    array("Nom" => "Collique", "Prenom" => "Al"),
+    array("Nom" => "Morris", "Prenom" => "Phillip"), 
+    array("Nom" => "D,eau", "Prenom" => "Deux bols"),
+    ),
+  );
 
 echo '10.Les eleves du college : <br>';
 //ajoutez votre code ici
+foreach ($college as $classe => $eleves){
+  echo $classe."<br><br>";
+  foreach ($eleves as $key => $value) {
+    foreach ($value as $value) {
+      echo $value."<br>";
+    }
+   echo "<br>";
+  }
+}
 echo '<br><br>';
 
 //----------------------------------------
@@ -162,6 +180,7 @@ echo '<br><br>';
 //Afficher toutes les informations de la vidéothèque
 $videotheque = array(
   array(
+
     'nom' => 'Independance day',
     'date' => 1996,
     'realisateur' => 'Roland Emmerich',
@@ -193,10 +212,49 @@ $videotheque = array(
       	'Henry Fonda','Martin Balsam','John Fiedler','Lee J. Cobb','E.G. Marshall',
     ),
   ),
+  array(
+    "nom" => "Shutter Island",
+    "date" => 2010,
+    "realisateur" => "Martin scorsese",
+    "synosis" => "En 1954, le marshal Teddy Daniels et son coéquipier Chuck Aule sont envoyés enquêter sur l'île de Shutter Island, dans un hôpital psychiatrique où sont internés de dangereux criminels. L'une des patientes, Rachel Solando, a inexplicablement disparu. Comment la meurtrière a-t-elle pu sortir d'une cellule fermée de l'extérieur ? Le seul indice retrouvé dans la pièce est une feuille de papier sur laquelle on peut lire une suite de chiffres et de lettres sans signification apparente. Oeuvre cohérente d'une malade, ou cryptogramme ?",
+    "acteurs" => array(
+      "Leonardo DiCaprio,", "Mark Ruffalo,", "Ben Kingsley,", "Michelle Williams,", "Emily Mortimer.",
+    ),
+  ),
+array(
+  "nom" => "Seven",
+  "date" => 1995,
+  "realisateur" => "David Fincher",
+  "synopsis" => "Pour conclure sa carrière, l'inspecteur Somerset, vieux flic blasé, tombe à sept jours de la retraite sur un criminel peu ordinaire. John Doe, c'est ainsi que se fait appeler l'assassin, a decidé de nettoyer la societé des maux qui la rongent en commettant sept meurtres basés sur les sept pechés capitaux: la gourmandise, l'avarice, la paresse, l'orgueil, la luxure, l'envie et la colère.",
+  "acteurs" => array( 
+    "Brad pitt,", "Morgan Freeman,", "Kevin Spacey,", "Gwyneth Paltrow.",
+  ),
+),
+array(
+  "nom" => "Le Seigneur des anneaux",
+  "date" => 2001,
+  "realisateur" => "Peter Jackson",
+  "synopsis" => "Dans ce chapitre de la trilogie, le jeune et timide Hobbit, Frodon Sacquet, hérite d'un anneau. Bien loin d'être une simple babiole, il s'agit de l'Anneau Unique, un instrument de pouvoir absolu qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu et de réduire en esclavage ses peuples. À moins que Frodon, aidé d'une Compagnie constituée de Hobbits, d'Hommes, d'un Magicien, d'un Nain, et d'un Elfe, ne parvienne à emporter l'Anneau à travers la Terre du Milieu jusqu'à la Crevasse du Destin, lieu où il a été forgé, et à le détruire pour toujours.",
+  "acteurs" => array(
+    "Ian McKellen,", "Viggo Mortensen,", "orlando Bloom,", "Elijah Wood.",
+  ),
+  ),
 );
 
 echo '12.Mes films : <br>';
 //ajoutez votre code ici
+foreach ($videotheque as $key => $film){
+  echo "<br><br>";
+  foreach ($film as $key1 => $value) {
+    if (!is_array($value)){
+    echo $value. " "."<br>";
+    }
+    foreach ($value as $value) {
+    echo $value. " ";
+    }
+    # code...
+  }
+}
 echo '<br><br>';
 
 //----------------------------------------
